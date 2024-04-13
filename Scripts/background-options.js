8303c38bf29aa45b7592c8d9055cb4e9602fe4d1
Bogdan Mihaica <claudiumihaica12@gmail.com>
2024-04-13 14:50:08 +0300

    questions responsive +c olors

--- Scripts/background-options.js
+++ Scripts/background-options.js
@@ -9,13 +9,14 @@ document.addEventListener("DOMContentLoaded", function () {
   let v1Toggle = false;
   let v2Toggle = false;
   let v3Toggle = false;
-  let selectColor = "#8c6464";
+  let selectColor = "#42c9a8";
+  let originalColor = "rgb(33, 177, 193)";
   variant1.addEventListener("click", (e) => {
     e.preventDefault();
     if (v1Toggle === false) {
       variant1.style.backgroundColor = selectColor;
     } else {
-      variant1.style.backgroundColor = "rgb(137, 67, 67)";
+      variant1.style.backgroundColor = originalColor;
     }
     v1Toggle = !v1Toggle;
     v1check.checked = v1Toggle;
@@ -26,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (v2Toggle === false) {
       variant2.style.backgroundColor = selectColor;
     } else {
-      variant2.style.backgroundColor = "rgb(137, 67, 67)";
+      variant2.style.backgroundColor = originalColor;
     }
     v2Toggle = !v2Toggle;
     v2check.checked = v2Toggle;
@@ -37,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (v3Toggle === false) {
       variant3.style.backgroundColor = selectColor;
     } else {
-      variant3.style.backgroundColor = "rgb(137, 67, 67)";
+      variant3.style.backgroundColor = originalColor;
     }
     v3Toggle = !v3Toggle;
     v3check.checked = v3Toggle;
