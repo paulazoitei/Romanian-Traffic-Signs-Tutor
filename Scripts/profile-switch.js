c357622707f4e2f3c910b16ba7f1bec9d73b1b33
Bogdan Mihaica <claudiumihaica12@gmail.com>
2024-04-14 22:00:12 +0300

    fixed auth

--- Scripts/profile-switch.js
+++ Scripts/profile-switch.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const submitLogin = document.getElementById("submit-login");
   submitLogin.addEventListener("click", (e) => {
     localStorage.setItem("logged", "true");
+    this.location.reload();
   });
 
   console.log(localStorage.getItem("logged"));
