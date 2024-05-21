6fe80917882683a5d9b0b307953d8d3ba37b065f
paulazoitei <azoitei_paul@yahoo.com>
2024-05-21 20:02:02 +0300

    mvc config

---
+++ app/controllers/home.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * The default home controller ,called when no controlloer/mehtod has been passed
+ * to the application.
+ *
+ * **/
+require_once __DIR__ . '/../core/Controller.php';
+
+class Home extends Controller
+{
+  public function index($name ='')
+  {
+     $user= $this->model('User');
+     $user->name=$name;
+
+     $this->view('home/index' , ['name' => $user->name]);
+
+
+  }
+
+}
+
